3d0596a2f4689bd24a8c06bbdb7b5b4af810d896
timothyn <tnguyen_42@yahoo.com>
2025-04-26 15:10:40 -0400

    Update to  files with some starter code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
   /*----- constants -----*/
-
+// Each of the "card" objects will be copied twice,
+// then shuffled and used for the board's cards
+const SOURCE_CARDS = [
+  {img: 'https://i.imgur.com/ZXPKaiN.jpg', matched: false},
+  {img: 'https://i.imgur.com/XMEsZBX.jpg', matched: false},
+  {img: 'https://i.imgur.com/6jX1bMT.jpg', matched: false},
+  {img: 'https://i.imgur.com/yKdqsBv.jpg', matched: false},
+  {img: 'https://i.imgur.com/1BV3HLr.jpg', matched: false},
+  {img: 'https://i.imgur.com/QYmN6Hp.jpg', matched: false},
+  {img: 'https://i.imgur.com/D5pWE05.jpg', matched: false},
+  {img: 'https://i.imgur.com/Ss4Xo3x.jpg', matched: false}
+];
+const CARD_BACK = 'https://i.imgur.com/WoEmI2M.jpg'; 
 
   /*----- state variables -----*/
 let board;
@@ -13,3 +25,7 @@ let winner;
 
 
   /*----- functions -----*/
+
+
+
+  // Initialize all state, then call render()
